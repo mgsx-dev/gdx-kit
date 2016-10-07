10ed3cea27a3e33635ce25c1f5ee0cbd77fb4a2b
mgsx-dev <germain.aubert@gmail.com>
2016-10-07 11:32:35 +0200

    LibGDX setup

---
+++ desktop/src/net/mgsx/box2d/editor/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package net.mgsx.box2d.editor.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import net.mgsx.box2d.editor.Box2DEditor;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Box2DEditor(), config);
+	}
+}
