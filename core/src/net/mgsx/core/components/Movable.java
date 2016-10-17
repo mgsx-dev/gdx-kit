b9c440e7156f82762c09190247da94134744af58
germ1 <germain.aubert@gmail.com>
2016-10-17 15:38:14 +0200

    doc

--- core/src/net/mgsx/core/components/Movable.java
+++ core/src/net/mgsx/core/components/Movable.java
@@ -8,6 +8,20 @@ import com.badlogic.gdx.math.Vector3;
 // in term of entity/component concept, a movable
 // is data (x,y,z) and could have a behavior for this data (track, player, btree ... and so on ...)
 //
+// TODO should be renamed Transform controller ..
+// we could imagine an object translated by a box2D body and rotated by an AI. How
+// to manage that with components :
+// a TranslationComponent with an optional Translation controller
+// a RotationComponent with an optional RotationController
+// idem for scale ?
+//
+// in this way an entity can have one and only one translation controller at a time.
+// but a special controller can have many other controllers which it sum up to set final position.
+// 
+// summary :
+// Controller <- TransformControl <- CompositeTransformControl (as in blender ...)
+
+
 public class Movable implements Component
 {
 	private Movable delegate;
--- core/src/net/mgsx/plugins/btree/BTreePlugin.java
+++ core/src/net/mgsx/plugins/btree/BTreePlugin.java
@@ -1,14 +1,60 @@
 package net.mgsx.plugins.btree;
 
+import com.badlogic.ashley.core.Component;
+import com.badlogic.ashley.core.Entity;
+import com.badlogic.gdx.ai.btree.BehaviorTree;
+import com.badlogic.gdx.ai.btree.LeafTask;
+import com.badlogic.gdx.ai.btree.Task;
+import com.badlogic.gdx.ai.btree.utils.BehaviorTreeLibraryManager;
+
 import net.mgsx.core.Editor;
 import net.mgsx.core.plugins.EditorPlugin;
 import net.mgsx.core.storage.Storage;
 
 public class BTreePlugin extends EditorPlugin
 {
+	// TODO
+	// how to link behavior tree (typed) with entity/component :
+	// solution 1 :
+	// all Task apply to entity (which is good for reuse !)
+	// a task adapter for easy coding
+	
+	public abstract static class EntityLeafTask extends LeafTask<Entity>{
+		@Override
+		public Status execute() {
+			return null;
+		}
+		@Override
+		protected Task<Entity> copyTo(Task<Entity> task) {
+			return task;
+		}
+	}
+	
+	public abstract static class BTreeComponentTask<T extends Component> extends EntityLeafTask
+	{
+		private Class<T> type;
+		public BTreeComponentTask(Class<T> type) {
+			super();
+			this.type = type;
+		}
+		@Override
+		public Status execute() 
+		{
+			return execute(getObject().getComponent(type));
+		}
+		abstract public Status execute(T model);
+		@Override
+		protected Task<Entity> copyTo(Task<Entity> task) {
+			return task;
+		}
+	}
+	
+	
 	@Override
 	public void initialize(Editor editor) 
 	{
 		Storage.register(BTreeModel.class, "btree");
 	}
+	
+	
 }
