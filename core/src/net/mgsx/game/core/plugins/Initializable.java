79c98b3fc8cf4d4b514a822d102a767922ca3fa5
germ1 <germain.aubert@gmail.com>
2016-11-07 12:59:04 +0100

    doc

--- core/src/net/mgsx/game/core/plugins/Initializable.java
+++ core/src/net/mgsx/game/core/plugins/Initializable.java
@@ -3,6 +3,8 @@ package net.mgsx.game.core.plugins;
 import com.badlogic.ashley.core.Engine;
 import com.badlogic.ashley.core.Entity;
 
-public interface Initializable {
+public interface Initializable 
+{
+	// TODO rename ECSAware ? or EntityAwareComponent or EntityAwareComponent and EngineAwareComponent
 	public void initialize(Engine manager, Entity entity);
 }
