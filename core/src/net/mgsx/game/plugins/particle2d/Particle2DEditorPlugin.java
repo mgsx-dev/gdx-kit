b8947166d436a609454c5a6175c60b78b3764fad
germ1 <germain.aubert@gmail.com>
2016-11-24 13:04:23 +0100

    fix particle pool after editor reset

--- core/src/net/mgsx/game/plugins/particle2d/Particle2DEditorPlugin.java
+++ core/src/net/mgsx/game/plugins/particle2d/Particle2DEditorPlugin.java
@@ -1,9 +1,11 @@
 package net.mgsx.game.plugins.particle2d;
 
 import com.badlogic.gdx.graphics.g2d.ParticleEffect;
+import com.badlogic.gdx.utils.Pool;
 
 import net.mgsx.game.core.EditorScreen;
 import net.mgsx.game.core.annotations.PluginDef;
+import net.mgsx.game.core.helpers.EditorAssetManager.AssetManagerListener;
 import net.mgsx.game.core.plugins.EditorPlugin;
 import net.mgsx.game.plugins.particle2d.tools.P2DBoundaryTool;
 import net.mgsx.game.plugins.particle2d.tools.P2DImportTool;
@@ -13,7 +15,7 @@ import net.mgsx.game.plugins.particle2d.tools.Particle2DReloader;
 public class Particle2DEditorPlugin extends EditorPlugin
 {
 	@Override
-	public void initialize(EditorScreen editor) 
+	public void initialize(final EditorScreen editor) 
 	{
 		// import tool
 		editor.addTool(new P2DImportTool(editor));
@@ -21,5 +23,24 @@ public class Particle2DEditorPlugin extends EditorPlugin
 		
 		// reloader
 		editor.assets.addReloadListener(ParticleEffect.class, new Particle2DReloader(editor, editor.registry.getPlugin(Particle2DPlugin.class)));
+	
+		editor.assets.addListener(new AssetManagerListener() {
+			@Override
+			public void removed(String fileName) {
+				changed(fileName);
+			}
+			@Override
+			public void changed(String fileName) {
+				Pool pool = editor.registry.getPlugin(Particle2DPlugin.class).pools.get(fileName);
+				if(pool != null){
+					pool.clear();
+					editor.registry.getPlugin(Particle2DPlugin.class).pools.remove(fileName);
+				}
+			}
+			@Override
+			public void added(String fileName, Class type) {
+			}
+		});
+
 	}
 }
