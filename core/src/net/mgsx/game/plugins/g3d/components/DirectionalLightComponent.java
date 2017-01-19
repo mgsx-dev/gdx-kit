594d501fd497ee2b7271c6993218a352472bf304
Germain AUBERT <germain.aubert@gmail.com>
2017-01-19 09:29:04 +0100

    fix light cloning

--- core/src/net/mgsx/game/plugins/g3d/components/DirectionalLightComponent.java
+++ core/src/net/mgsx/game/plugins/g3d/components/DirectionalLightComponent.java
@@ -2,6 +2,7 @@ package net.mgsx.game.plugins.g3d.components;
 
 import com.badlogic.ashley.core.Component;
 import com.badlogic.ashley.core.ComponentMapper;
+import com.badlogic.ashley.core.Engine;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
 import com.badlogic.gdx.math.Quaternion;
@@ -10,10 +11,11 @@ import com.badlogic.gdx.utils.Pool.Poolable;
 import net.mgsx.game.core.annotations.Editable;
 import net.mgsx.game.core.annotations.EditableComponent;
 import net.mgsx.game.core.annotations.Storable;
+import net.mgsx.game.core.components.Duplicable;
 
 @Storable(value="g3d.light.dir", auto=true)
-@EditableComponent(autoClone=true)
-public class DirectionalLightComponent implements Component, Poolable
+@EditableComponent
+public class DirectionalLightComponent implements Component, Poolable, Duplicable
 {
 	
 	public final static ComponentMapper<DirectionalLightComponent> components = ComponentMapper.getFor(DirectionalLightComponent.class);
@@ -29,6 +31,16 @@ public class DirectionalLightComponent implements Component, Poolable
 	public void reset() {
 		light = new DirectionalLight().set(Color.WHITE, 0, 0, -1);
 		shadow = false;
+	}
+
+	@Override
+	public Component duplicate(Engine engine)
+	{
+		DirectionalLightComponent clone = engine.createComponent(DirectionalLightComponent.class);
+		clone.direction.set(direction);
+		clone.light = new DirectionalLight().set(light);
+		clone.shadow = shadow;
+		return clone;
 	}
 	
 }
--- core/src/net/mgsx/game/plugins/g3d/components/PointLightComponent.java
+++ core/src/net/mgsx/game/plugins/g3d/components/PointLightComponent.java
@@ -2,6 +2,7 @@ package net.mgsx.game.plugins.g3d.components;
 
 import com.badlogic.ashley.core.Component;
 import com.badlogic.ashley.core.ComponentMapper;
+import com.badlogic.ashley.core.Engine;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g3d.environment.PointLight;
 import com.badlogic.gdx.utils.Pool.Poolable;
@@ -9,10 +10,11 @@ import com.badlogic.gdx.utils.Pool.Poolable;
 import net.mgsx.game.core.annotations.Editable;
 import net.mgsx.game.core.annotations.EditableComponent;
 import net.mgsx.game.core.annotations.Storable;
+import net.mgsx.game.core.components.Duplicable;
 
 @Storable(value="g3d.light.point", auto=true)
-@EditableComponent(autoClone=true)
-public class PointLightComponent implements Component, Poolable
+@EditableComponent
+public class PointLightComponent implements Component, Poolable, Duplicable
 {
 	
 	public final static ComponentMapper<PointLightComponent> components = ComponentMapper.getFor(PointLightComponent.class);
@@ -25,5 +27,11 @@ public class PointLightComponent implements Component, Poolable
 		light = new PointLight().setColor(Color.WHITE).setIntensity(100);
 	}
 	
-	
+	@Override
+	public Component duplicate(Engine engine)
+	{
+		PointLightComponent clone = engine.createComponent(PointLightComponent.class);
+		clone.light = new PointLight().set(light);
+		return clone;
+	}
 }
