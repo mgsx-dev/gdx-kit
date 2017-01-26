07429a46c098b622b7cf72a3a01c9193dd8ea07a
Germain AUBERT <germain.aubert@gmail.com>
2017-01-26 13:02:48 +0100

    fix plugin registration

--- demo-desktop/src/net/mgsx/examples/platformer/CraftingEditorLauncher.java
+++ demo-desktop/src/net/mgsx/examples/platformer/CraftingEditorLauncher.java
@@ -23,7 +23,10 @@ public class CraftingEditorLauncher {
 
 	public static void main (String[] args) 
 	{
-		ClassRegistry.instance = new ReflectionClassRegistry();
+		ClassRegistry.instance = new ReflectionClassRegistry(
+				ReflectionClassRegistry.kitCore,
+				ReflectionClassRegistry.kitCorePlugin,
+				"net.mgsx.examples.crafting");
 		
 		DesktopNativeInterface nativeService = new DesktopNativeInterface(); 
 		NativeService.instance = nativeService; 
