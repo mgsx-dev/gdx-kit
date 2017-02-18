fdb309c6cd41bc8f8bb5559cbab3b9b95a6605a6
Germain AUBERT <germain.aubert@gmail.com>
2017-02-18 18:42:01 +0100

    TODOs

--- core/src/net/mgsx/game/core/storage/ContextualSerializer.java
+++ core/src/net/mgsx/game/core/storage/ContextualSerializer.java
@@ -3,6 +3,15 @@ package net.mgsx.game.core.storage;
 import com.badlogic.ashley.core.Entity;
 import com.badlogic.gdx.utils.Json;
 
+// TODO not enough : linked entity may appear after current entity and not available
+// at deserialization time. Requires another mecanism : "Resolver" which is run
+// after deserialization and apply on already parsed component.
+// this simplify things a bit : all fields are serialized normal way exept
+// field marked as @Resolved, these fields are pushed to a context with id
+// and resolved by resolver at resolve time. Maybe do it automatically for any
+// Entity field !!
+// More : ContextualDuplicable is also impacted and could be simplifyed ... as well
+//
 public abstract class ContextualSerializer<T> implements Json.Serializer<T> 
 {
 	protected EntityGroup context;
