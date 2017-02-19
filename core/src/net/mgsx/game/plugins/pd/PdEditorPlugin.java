0e61696ece077567d9c1a5054d71c35cee77313f
Germain AUBERT <germain.aubert@gmail.com>
2017-02-19 17:19:05 +0100

    remove PD editor
    not working because of audio is enabled/disabled at launch time and
    switch cannot be performed when running.

--- core/src/net/mgsx/game/plugins/pd/PdEditorPlugin.java
+++ core/src/net/mgsx/game/plugins/pd/PdEditorPlugin.java
@@ -3,7 +3,6 @@ package net.mgsx.game.plugins.pd;
 import net.mgsx.game.core.EditorScreen;
 import net.mgsx.game.core.annotations.PluginDef;
 import net.mgsx.game.core.plugins.EditorPlugin;
-import net.mgsx.game.plugins.pd.editors.PdEditor;
 
 @PluginDef(dependencies=PdPlugin.class)
 public class PdEditorPlugin extends EditorPlugin
@@ -12,7 +11,6 @@ public class PdEditorPlugin extends EditorPlugin
 	@Override
 	public void initialize(EditorScreen editor) 
 	{
-		editor.registry.addGlobalEditor("Pd", new PdEditor());
 	}
 
 }
