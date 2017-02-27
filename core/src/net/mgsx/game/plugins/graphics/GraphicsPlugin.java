10c370df517df6377a09a45cc123d47596232c02
Germain AUBERT <germain.aubert@gmail.com>
2017-02-27 23:44:16 +0100

    fix serialization issue

--- core/src/net/mgsx/game/plugins/graphics/GraphicsPlugin.java
+++ core/src/net/mgsx/game/plugins/graphics/GraphicsPlugin.java
@@ -1,7 +1,9 @@
 package net.mgsx.game.plugins.graphics;
 
 import net.mgsx.game.core.GameScreen;
+import net.mgsx.game.core.helpers.FilesShaderProgram;
 import net.mgsx.game.core.plugins.Plugin;
+import net.mgsx.game.plugins.graphics.storage.FilesShaderProgramSerializer;
 import net.mgsx.game.plugins.graphics.systems.ScreenSystem;
 
 public class GraphicsPlugin implements Plugin
@@ -9,6 +11,7 @@ public class GraphicsPlugin implements Plugin
 
 	@Override
 	public void initialize(GameScreen engine) {
+		engine.registry.addSerializer(FilesShaderProgram.class, new FilesShaderProgramSerializer());
 		engine.entityEngine.addSystem(new ScreenSystem(engine));
 	}
 
