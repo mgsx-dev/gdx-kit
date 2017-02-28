799ec912bb1683b22f7fa820f7f27ed1526c2b09
Germain AUBERT <germain.aubert@gmail.com>
2017-02-28 08:36:51 +0100

    add batch lock/unlock tool

---
+++ core/src/net/mgsx/game/plugins/core/tools/LockTool.java
@@ -0,0 +1,37 @@
+package net.mgsx.game.plugins.core.tools;
+
+import com.badlogic.ashley.core.Entity;
+import com.badlogic.gdx.Input;
+
+import net.mgsx.game.core.EditorScreen;
+import net.mgsx.game.core.tools.Tool;
+import net.mgsx.game.plugins.core.components.Locked;
+
+public class LockTool extends Tool
+{
+
+	public LockTool(EditorScreen editor) {
+		super("Lock/Unlock", editor);
+	}
+	
+	@Override
+	public boolean keyDown(int keycode) 
+	{
+		if(ctrl() && keycode == Input.Keys.L)
+		{
+			if(shift()){
+				for(Entity entity : selection().selection){
+					entity.remove(Locked.class);
+				}
+			}else{
+				for(Entity entity : selection().selection){
+					entity.add(getEngine().createComponent(Locked.class));
+				}
+			}
+			
+			return true;
+		}
+		return super.keyDown(keycode);
+	}
+
+}
