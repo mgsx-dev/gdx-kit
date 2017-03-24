7113e661e7241cf3d2834ddb8e9021c08fb05cfd
Germain AUBERT <germain.aubert@gmail.com>
2017-03-24 10:11:14 +0100

    add missing components references

--- core/src/net/mgsx/game/plugins/camera/CameraPlugin.java
+++ core/src/net/mgsx/game/plugins/camera/CameraPlugin.java
@@ -13,14 +13,16 @@ import com.badlogic.gdx.utils.viewport.Viewport;
 import net.mgsx.game.core.GameScreen;
 import net.mgsx.game.core.annotations.PluginDef;
 import net.mgsx.game.core.plugins.Plugin;
+import net.mgsx.game.plugins.camera.components.ActiveCamera;
 import net.mgsx.game.plugins.camera.components.CameraComponent;
+import net.mgsx.game.plugins.camera.components.ViewportComponent;
 import net.mgsx.game.plugins.camera.storage.CameraSerializer;
 import net.mgsx.game.plugins.camera.storage.ViewportSerializer;
 import net.mgsx.game.plugins.camera.systems.CameraSystem;
 import net.mgsx.game.plugins.camera.systems.CameraTransformSystem;
 
 @PluginDef(category="graphics", components={
-		CameraComponent.class})
+		CameraComponent.class, ActiveCamera.class, ViewportComponent.class})
 public class CameraPlugin implements Plugin
 {
 	@Override
