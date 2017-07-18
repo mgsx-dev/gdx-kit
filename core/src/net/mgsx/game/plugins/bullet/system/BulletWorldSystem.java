72f57aab406b879ec3e3483b4acd2f8c13ff7cd9
Germain AUBERT <germain.aubert@gmail.com>
2017-07-18 12:07:04 +0200

    add bullet helpers : rayCast and simple body creation

--- core/src/net/mgsx/game/plugins/bullet/system/BulletWorldSystem.java
+++ core/src/net/mgsx/game/plugins/bullet/system/BulletWorldSystem.java
@@ -1,20 +1,30 @@
 package net.mgsx.game.plugins.bullet.system;
 
 import com.badlogic.ashley.core.Engine;
+import com.badlogic.ashley.core.Entity;
 import com.badlogic.ashley.core.EntitySystem;
+import com.badlogic.gdx.math.Matrix4;
 import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.math.collision.Ray;
+import com.badlogic.gdx.physics.bullet.collision.ClosestRayResultCallback;
+import com.badlogic.gdx.physics.bullet.collision.btBoxShape;
 import com.badlogic.gdx.physics.bullet.collision.btBroadphaseInterface;
 import com.badlogic.gdx.physics.bullet.collision.btCollisionConfiguration;
 import com.badlogic.gdx.physics.bullet.collision.btCollisionDispatcher;
+import com.badlogic.gdx.physics.bullet.collision.btCollisionObject;
 import com.badlogic.gdx.physics.bullet.collision.btCollisionWorld;
 import com.badlogic.gdx.physics.bullet.collision.btDbvtBroadphase;
 import com.badlogic.gdx.physics.bullet.collision.btDefaultCollisionConfiguration;
 import com.badlogic.gdx.physics.bullet.dynamics.btConstraintSolver;
 import com.badlogic.gdx.physics.bullet.dynamics.btDiscreteDynamicsWorld;
 import com.badlogic.gdx.physics.bullet.dynamics.btDynamicsWorld;
+import com.badlogic.gdx.physics.bullet.dynamics.btRigidBody;
+import com.badlogic.gdx.physics.bullet.dynamics.btRigidBody.btRigidBodyConstructionInfo;
 import com.badlogic.gdx.physics.bullet.dynamics.btSequentialImpulseConstraintSolver;
+import com.badlogic.gdx.physics.bullet.linearmath.btMotionState;
 
 import net.mgsx.game.core.GamePipeline;
+import net.mgsx.game.plugins.bullet.components.BulletComponent;
 
 public class BulletWorldSystem extends EntitySystem
 {
@@ -49,6 +59,65 @@ public class BulletWorldSystem extends EntitySystem
 	{
 		((btDynamicsWorld)collisionWorld).stepSimulation(deltaTime, maxSubSteps, fixedTimeStep);
 
+	}
+	
+	/**
+	 * 
+	 * @param rayFrom initial ray, clipped at collision distance (if collides)
+	 * @param rayResult position/normal at ray collision (if collides)
+	 * @return the hit entity or null if no collision
+	 */
+	public Entity rayCast(Ray rayFrom, Ray rayResult){
+		ClosestRayResultCallback resultCallback = new ClosestRayResultCallback(Vector3.Zero.cpy(), Vector3.Z.cpy());
+		collisionWorld.rayTest(rayFrom.origin, rayFrom.origin.cpy().add(rayFrom.direction), resultCallback);
+		boolean hasHit = resultCallback.hasHit();
+		if(hasHit){
+			Vector3 p = new Vector3();
+			resultCallback.getHitPointWorld(p);
+			rayFrom.direction.scl(resultCallback.getClosestHitFraction());
+			resultCallback.getHitNormalWorld(rayResult.direction);
+			rayResult.origin.set(rayFrom.origin).add(rayFrom.direction);
+		}
+		resultCallback.release();
+		return hasHit ? (Entity)resultCallback.getCollisionObject().userData : null;
+	}
+
+	public void createBox(Entity entity, final Matrix4 transform, float w, float h, float d, boolean dynamic) {
+		BulletComponent bullet = getEngine().createComponent(BulletComponent.class);
+		
+		
+		bullet.shape = new btBoxShape(new Vector3(w,h,d).scl(0.5f));
+		
+		if(dynamic)
+			bullet.object = new btRigidBody(new btRigidBodyConstructionInfo(10, new btMotionState(){
+				@Override
+				public void getWorldTransform(Matrix4 worldTrans) {
+					worldTrans.set(transform);
+				}
+				@Override
+				public void setWorldTransform(Matrix4 worldTrans) {
+					transform.set(worldTrans);
+				}
+			}, bullet.shape, new Vector3(1,1,1)));
+		else{
+			bullet.object = new btCollisionObject();
+			bullet.object.setCollisionShape(bullet.shape);
+			bullet.object.setWorldTransform(transform);
+		}
+		
+		bullet.world = collisionWorld;
+		bullet.object.setFriction(.99f);
+		bullet.object.setRestitution(0.01f);
+		bullet.object.userData = entity;
+		
+		if(dynamic){
+			((btDiscreteDynamicsWorld)collisionWorld).addRigidBody((btRigidBody)bullet.object);
+		}else{
+			collisionWorld.addCollisionObject(bullet.object);
+		}
+		
+		entity.add(bullet);
+		
 	}
 
 }
