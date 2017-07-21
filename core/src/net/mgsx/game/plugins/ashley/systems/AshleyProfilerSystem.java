2072c835119ad8e5c30f77822e2b9dbfb585c04d
Germain AUBERT <germain.aubert@gmail.com>
2017-07-21 11:43:53 +0200

    add basic memory monitoring

--- core/src/net/mgsx/game/plugins/ashley/systems/AshleyProfilerSystem.java
+++ core/src/net/mgsx/game/plugins/ashley/systems/AshleyProfilerSystem.java
@@ -2,9 +2,11 @@ package net.mgsx.game.plugins.ashley.systems;
 
 import com.badlogic.ashley.core.EntitySystem;
 import com.badlogic.ashley.core.PooledEngine;
+import com.badlogic.gdx.math.MathUtils;
 
 import net.mgsx.game.core.annotations.Editable;
 import net.mgsx.game.core.annotations.EditableSystem;
+import net.mgsx.game.core.annotations.EnumType;
 
 @EditableSystem
 public class AshleyProfilerSystem extends EntitySystem
@@ -14,6 +16,17 @@ public class AshleyProfilerSystem extends EntitySystem
 	
 	@Editable(realtime=true, readonly=true)
 	public int systems;
+	
+	@Editable(realtime=true, readonly=true, type=EnumType.BYTES)
+	public float memory;
+	
+	@Editable(realtime=true, readonly=true, type=EnumType.BYTES)
+	public float memoryRate;
+	
+	private transient long prevMemory;
+	private transient long consumedMemory;
+	private transient float time;
+	
 	
 	@Editable
 	public void clearPools(){
@@ -29,9 +42,32 @@ public class AshleyProfilerSystem extends EntitySystem
 		setProcessing(false); // TODO config
 	}
 	
+	@Editable
+	public void forceGC(){
+		Runtime.getRuntime().gc();
+	}
+	
 	@Override
 	public void update(float deltaTime) {
 		entities = getEngine().getEntities().size();
 		systems = getEngine().getSystems().size();
+		
+		time += deltaTime;
+		
+		long currentMemoryUsage = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
+		long deltaMemory = currentMemoryUsage - prevMemory;
+		if(deltaMemory > 0){
+			consumedMemory += currentMemoryUsage - prevMemory;
+			if(time > 0){
+				memoryRate = consumedMemory / time;
+			}
+		}else if(deltaMemory < 0){
+			consumedMemory = 0;
+			time = 0;
+		}
+		prevMemory = currentMemoryUsage;
+		
+		
+		memory = MathUtils.floor(currentMemoryUsage);
 	}
 }
