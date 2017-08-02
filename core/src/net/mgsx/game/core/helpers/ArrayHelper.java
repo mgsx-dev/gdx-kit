27d29987434f9b88a7773e086765fddf71a98d3a
Germain AUBERT <germain.aubert@gmail.com>
2017-08-02 14:09:01 +0200

    array random helper

--- core/src/net/mgsx/game/core/helpers/ArrayHelper.java
+++ core/src/net/mgsx/game/core/helpers/ArrayHelper.java
@@ -1,5 +1,6 @@
 package net.mgsx.game.core.helpers;
 
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.utils.Array;
 
 public class ArrayHelper {
@@ -19,5 +20,11 @@ public class ArrayHelper {
 		Array<T> array = new Array<T>();
 		for(T element : elements) array.add(element);
 		return array;
+	}
+	public static <T> T any(Array<T> array) {
+		if(array.size > 0){
+			return array.get((int)(array.size * MathUtils.random()));
+		}
+		return null;
 	}
 }
