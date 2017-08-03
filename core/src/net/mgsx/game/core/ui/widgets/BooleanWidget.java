7f0eef09e04bf0a8dffd30decbfbdb65c33c2743
Germain AUBERT <germain.aubert@gmail.com>
2017-08-03 14:43:32 +0200

    allow realtime boolean

--- core/src/net/mgsx/game/core/ui/widgets/BooleanWidget.java
+++ core/src/net/mgsx/game/core/ui/widgets/BooleanWidget.java
@@ -1,6 +1,8 @@
 package net.mgsx.game.core.ui.widgets;
 
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
@@ -13,39 +15,41 @@ abstract public class BooleanWidget implements FieldEditor
 	public static final BooleanWidget instance = labeled("true", "false");
 	
 	public static BooleanWidget labeled(final String labelOnOff){
-		return new BooleanWidget(){
-			@Override
-			public Actor create(final Accessor accessor, Skin skin) {
-				final TextButton button = new TextButton(labelOnOff, skin, "toggle");
-				button.setChecked((Boolean)accessor.get());
-				button.addListener(new ChangeListener() {
-					@Override
-					public void changed(ChangeEvent event, Actor actor) {
-						if((Boolean)accessor.get() != button.isChecked()){
-							accessor.set(button.isChecked());
-						}
-					}
-				});
-				return button;
-			}
-		};
+		return labeled(labelOnOff, labelOnOff);
 	}
 	public static BooleanWidget labeled(final String labelOn, final String labelOff){
 		return new BooleanWidget(){
 			@Override
 			public Actor create(final Accessor accessor, Skin skin) {
-				final TextButton button = new TextButton((Boolean)accessor.get() ? labelOn : labelOff, skin, "toggle");
-				button.setChecked((Boolean)accessor.get());
-				button.addListener(new ChangeListener() {
-					@Override
-					public void changed(ChangeEvent event, Actor actor) {
-						if((Boolean)accessor.get() != button.isChecked()){
-							accessor.set(button.isChecked());
-							button.setText((Boolean)accessor.get() ? labelOn : labelOff);
+				final boolean realtime = accessor.config() != null && accessor.config().realtime();
+				final boolean readonly = accessor.config() != null && accessor.config().readonly();
+				if(readonly){
+					final Label label = new Label((Boolean)accessor.get() ? labelOn : labelOff, skin){
+						@Override
+						public void act(float delta) {
+							if(realtime){
+								boolean value = accessor.get(Boolean.class);
+								setText(value ? labelOn : labelOff);
+							}
+							super.act(delta);
 						}
-					}
-				});
+					};
+					label.setColor(Color.CYAN);
+					return label;
+				}else{
+					final TextButton button = new TextButton((Boolean)accessor.get() ? labelOn : labelOff, skin, "toggle");
+					button.setChecked((Boolean)accessor.get());
+					button.addListener(new ChangeListener() {
+						@Override
+						public void changed(ChangeEvent event, Actor actor) {
+							if((Boolean)accessor.get() != button.isChecked()){
+								accessor.set(button.isChecked());
+								button.setText((Boolean)accessor.get() ? labelOn : labelOff);
+							}
+						}
+					});
 				return button;
+				}
 			}
 		};
 	}
