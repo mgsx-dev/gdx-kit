e0b18d6aa7687e3c301e1bfa9bab90b6eea8c702
Germain AUBERT <germain.aubert@gmail.com>
2017-08-05 13:39:33 +0200

    bullet object creation based on density

--- core/src/net/mgsx/game/plugins/bullet/system/BulletWorldSystem.java
+++ core/src/net/mgsx/game/plugins/bullet/system/BulletWorldSystem.java
@@ -3,7 +3,9 @@ package net.mgsx.game.plugins.bullet.system;
 import com.badlogic.ashley.core.Engine;
 import com.badlogic.ashley.core.Entity;
 import com.badlogic.ashley.core.EntitySystem;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g3d.Model;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Matrix4;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.math.collision.BoundingBox;
@@ -23,7 +25,6 @@ import com.badlogic.gdx.physics.bullet.dynamics.btConstraintSolver;
 import com.badlogic.gdx.physics.bullet.dynamics.btDiscreteDynamicsWorld;
 import com.badlogic.gdx.physics.bullet.dynamics.btDynamicsWorld;
 import com.badlogic.gdx.physics.bullet.dynamics.btRigidBody;
-import com.badlogic.gdx.physics.bullet.dynamics.btRigidBody.btRigidBodyConstructionInfo;
 import com.badlogic.gdx.physics.bullet.dynamics.btSequentialImpulseConstraintSolver;
 import com.badlogic.gdx.physics.bullet.linearmath.btMotionState;
 
@@ -32,6 +33,10 @@ import net.mgsx.game.plugins.bullet.components.BulletComponent;
 
 public class BulletWorldSystem extends EntitySystem
 {
+	/** bullet doesn't work well for tiny dynamic object see : https://code.google.com/archive/p/bullet/issues/45 */
+	public static final float HARD_MARGIN = 0.04f;
+	public static final float SAFE_MARGIN = 0.1f;
+	
 	public btCollisionConfiguration collisionConfiguration;
 	public btCollisionDispatcher dispatcher;
 	public btBroadphaseInterface broadphase;
@@ -91,14 +96,31 @@ public class BulletWorldSystem extends EntitySystem
 		return hasHit ? resultCallback.getCollisionObject().userData : null;
 	}
 
-	public void createBox(Entity entity, final Matrix4 transform, float w, float h, float d, boolean dynamic) {
+	/**
+	 * 
+	 * @param entity
+	 * @param transform
+	 * @param w unit : m
+	 * @param h unit : m
+	 * @param d unit : m
+	 * @param dynamic 
+	 * @param density unit : Kg/m³ (only used for dynamic bodies)
+	 */
+	public void createBox(Entity entity, final Matrix4 transform, float w, float h, float d, boolean dynamic, float density) {
 		BulletComponent bullet = getEngine().createComponent(BulletComponent.class);
-		
 		
 		bullet.shape = new btBoxShape(new Vector3(w,h,d).scl(0.5f));
 		
-		if(dynamic)
-			bullet.object = new btRigidBody(new btRigidBodyConstructionInfo(10, new btMotionState(){
+		if(dynamic){
+			if(w < SAFE_MARGIN || h < SAFE_MARGIN || d<SAFE_MARGIN){
+				Gdx.app.log("KitBullet", "warning : object is very small boundary box " + w + ", " + h + ", " + d);
+			}
+			bullet.density = density;
+			float volume = bullet.volume = w * h * d;
+			float mass = bullet.mass = density * volume;
+			Vector3 inertia = bullet.inertia;
+			bullet.shape.calculateLocalInertia(mass , inertia);
+			bullet.object = new btRigidBody(mass, new btMotionState(){
 				@Override
 				public void getWorldTransform(Matrix4 worldTrans) {
 					worldTrans.set(transform);
@@ -107,7 +129,8 @@ public class BulletWorldSystem extends EntitySystem
 				public void setWorldTransform(Matrix4 worldTrans) {
 					transform.set(worldTrans);
 				}
-			}, bullet.shape, new Vector3(1,1,1)));
+			}, bullet.shape, inertia);
+		}
 		else{
 			bullet.object = new btCollisionObject();
 			bullet.object.setCollisionShape(bullet.shape);
@@ -115,8 +138,6 @@ public class BulletWorldSystem extends EntitySystem
 		}
 		
 		bullet.world = collisionWorld;
-		bullet.object.setFriction(.99f);
-		bullet.object.setRestitution(0.01f);
 		bullet.object.userData = entity;
 		
 		if(dynamic){
@@ -129,14 +150,31 @@ public class BulletWorldSystem extends EntitySystem
 		
 	}
 
-	public void createSphere(Entity entity, final Matrix4 transform, float r, boolean dynamic) {
+	/**
+	 * 
+	 * @param entity
+	 * @param transform
+	 * @param r radius unit : m
+	 * @param dynamic
+	 * @param density unit : Kg/m³
+	 */
+	public void createSphere(Entity entity, final Matrix4 transform, float r, boolean dynamic, float density) {
 		BulletComponent bullet = getEngine().createComponent(BulletComponent.class);
 		
 		
 		bullet.shape = new btSphereShape(r);
 		
 		if(dynamic)
-			bullet.object = new btRigidBody(new btRigidBodyConstructionInfo(10, new btMotionState(){
+		{
+			if(r*2 < SAFE_MARGIN){
+				Gdx.app.log("KitBullet", "warning : object is very small radius " + r);
+			}
+			bullet.density = density;
+			float volume = bullet.volume = MathUtils.PI * r * r *r * 4f/3f;
+			float mass = bullet.mass = density * volume;
+			Vector3 inertia = bullet.inertia;
+			bullet.shape.calculateLocalInertia(mass , inertia);
+			bullet.object = new btRigidBody(10, new btMotionState(){
 				@Override
 				public void getWorldTransform(Matrix4 worldTrans) {
 					worldTrans.set(transform);
@@ -145,16 +183,14 @@ public class BulletWorldSystem extends EntitySystem
 				public void setWorldTransform(Matrix4 worldTrans) {
 					transform.set(worldTrans);
 				}
-			}, bullet.shape, new Vector3(1,1,1)));
-		else{
+			}, bullet.shape, inertia);
+		}else{
 			bullet.object = new btCollisionObject();
 			bullet.object.setCollisionShape(bullet.shape);
 			bullet.object.setWorldTransform(transform);
 		}
 		
 		bullet.world = collisionWorld;
-		bullet.object.setFriction(.99f);
-		bullet.object.setRestitution(0.01f);
 		bullet.object.userData = entity;
 		
 		if(dynamic){
@@ -166,7 +202,17 @@ public class BulletWorldSystem extends EntitySystem
 		entity.add(bullet);
 	}
 
-	public void createFromModel(Entity entity, Model model, final Matrix4 transform, boolean dynamic) {
+	/**
+	 * note : for dynamic bodies, shape is roughly approximated to the model boundary box max extends. Therefore,
+	 * model should be centered at zero.
+	 * TODO : provides other methods of approximation (sphere, a low res convex hull mesh)
+	 * @param entity
+	 * @param model
+	 * @param transform
+	 * @param dynamic
+	 * @param density unit : Kg/m³
+	 */
+	public void createFromModel(Entity entity, Model model, final Matrix4 transform, boolean dynamic, float density) {
 		BulletComponent bullet = getEngine().createComponent(BulletComponent.class);
 		
 		// TODO add dynamic support ?? not really possible natively by bullet
@@ -176,8 +222,20 @@ public class BulletWorldSystem extends EntitySystem
 		// more accurate with center offset ...etc
 		
 		if(dynamic){
-			bullet.shape = new btBoxShape(model.calculateBoundingBox(new BoundingBox()).max);
-			bullet.object = new btRigidBody(new btRigidBodyConstructionInfo(10, new btMotionState(){
+			BoundingBox bbox = model.calculateBoundingBox(new BoundingBox());
+			float w = bbox.max.x*2;
+			float h = bbox.max.y*2;
+			float d = bbox.max.z*2;
+			if(w < SAFE_MARGIN || h < SAFE_MARGIN || d < SAFE_MARGIN){
+				Gdx.app.log("KitBullet", "warning : object is very small boundary box " + bbox.toString());
+			}
+			bullet.shape = new btBoxShape(bbox.max);
+			bullet.density = density;
+			float volume = bullet.volume = w * h * d;
+			float mass = bullet.mass = density * volume;
+			Vector3 inertia = bullet.inertia;
+			bullet.shape.calculateLocalInertia(mass , inertia);
+			bullet.object = new btRigidBody(10, new btMotionState(){
 				@Override
 				public void getWorldTransform(Matrix4 worldTrans) {
 					worldTrans.set(transform);
@@ -186,7 +244,7 @@ public class BulletWorldSystem extends EntitySystem
 				public void setWorldTransform(Matrix4 worldTrans) {
 					transform.set(worldTrans);
 				}
-			}, bullet.shape, new Vector3(1,1,1)));
+			}, bullet.shape, inertia);
 		}else{
 			bullet.shape = Bullet.obtainStaticNodeShape(model.nodes);
 			bullet.object = new btCollisionObject();
@@ -195,8 +253,6 @@ public class BulletWorldSystem extends EntitySystem
 		}
 		
 		bullet.world = collisionWorld;
-		bullet.object.setFriction(.99f);
-		bullet.object.setRestitution(0.01f);
 		bullet.object.userData = entity;
 		
 		if(dynamic){
