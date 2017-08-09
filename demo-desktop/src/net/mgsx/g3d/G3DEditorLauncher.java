c23f48209e14cc49d3373b1077d00146184e91f0
Germain AUBERT <germain.aubert@gmail.com>
2017-08-09 10:31:43 +0200

    fix plugin dependency

--- demo-desktop/src/net/mgsx/g3d/G3DEditorLauncher.java
+++ demo-desktop/src/net/mgsx/g3d/G3DEditorLauncher.java
@@ -7,6 +7,7 @@ import net.mgsx.game.core.EditorApplication;
 import net.mgsx.game.core.EditorConfiguration;
 import net.mgsx.game.plugins.ashley.AshleyEditorPlugin;
 import net.mgsx.game.plugins.g3d.G3DEditorPlugin;
+import net.mgsx.game.plugins.graphics.GraphicsEditorPlugin;
 
 public class G3DEditorLauncher {
 
@@ -17,6 +18,7 @@ public class G3DEditorLauncher {
 		EditorConfiguration editConfig = new EditorConfiguration();
 		
 		editConfig.plugins.add(new AshleyEditorPlugin());
+		editConfig.plugins.add(new GraphicsEditorPlugin());
 		editConfig.plugins.add(new G3DEditorPlugin());
 		
 		editConfig.path = args.length > 0 ? args[0] : null;
