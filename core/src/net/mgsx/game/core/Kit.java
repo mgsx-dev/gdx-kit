c6c26aa9ac40eae5d587b74632b234a6601bff99
Germain AUBERT <germain.aubert@gmail.com>
2017-08-30 15:34:07 +0200

    make Kit class gwt compatible

--- core/src/net/mgsx/game/core/Kit.java
+++ core/src/net/mgsx/game/core/Kit.java
@@ -1,17 +1,19 @@
 package net.mgsx.game.core;
 
+import com.badlogic.gdx.Application.ApplicationType;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputMultiplexer;
 import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.GdxRuntimeException;
+import com.badlogic.gdx.utils.reflect.ClassReflection;
 
 import net.mgsx.game.core.meta.KitMeta;
-import net.mgsx.game.core.meta.ReflectionCache;
 
 public class Kit {
 
 	public static final InputMultiplexer inputs = new InputMultiplexer();
 	
-	public static final KitMeta meta = new ReflectionCache();
+	public static KitMeta meta;
 	
 	/**
 	 * Game listeners allow hook in game workflow.
@@ -19,6 +21,18 @@ public class Kit {
 	public static final Array<KitGameListener> gameListeners = new Array<KitGameListener>();
 	
 	private static boolean exited = false;
+	
+	static{
+		if(Gdx.app.getType() == ApplicationType.WebGL){
+			meta = null;
+		}else{
+			try {
+				meta = (KitMeta)ClassReflection.newInstance(ClassReflection.forName("net.mgsx.game.core.meta.ReflectionCache"));
+			} catch (Exception e) {
+				throw new GdxRuntimeException(e);
+			}
+		}
+	}
 	
 	/**
 	 * Exit application, all gameListeners will be called.
